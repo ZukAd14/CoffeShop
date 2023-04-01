2217ff406317ef98a859abea15d1921215ad2e91
Adam <adam.zukowski471@gmail.com>
2023-04-01 20:12:33 +0200

    Add bootstrap

---
+++ src/js/settings.js
@@ -0,0 +1,32 @@
+export const select = {
+  templateOf: {
+
+  },
+  containerOf: {
+
+  },
+  nav: {
+
+  },
+
+};
+
+export const classNames = {
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
+};
+
+export const settings = {
+  db: {
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+    products: 'products',
+  },
+};
+
+export const templates = {
+
+};
